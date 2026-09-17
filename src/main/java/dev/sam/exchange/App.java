68c226a63381a1a5b2023ebd24dddfffb28698b5
Sam Christensen <samctensen@gmail.com>
2026-09-16 17:08:07 -0700

    feat: replay saved journals from the command line

--- src/main/java/dev/sam/exchange/App.java
+++ src/main/java/dev/sam/exchange/App.java
@@ -1,16 +1,36 @@
 package dev.sam.exchange;
 
-import dev.sam.exchange.engine.PlaceOrder;
-import dev.sam.exchange.engine.Side;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import dev.sam.exchange.engine.CommandResult;
+import dev.sam.exchange.engine.EngineCommand;
+import dev.sam.exchange.engine.ReplayResult;
+import dev.sam.exchange.engine.ReplayRunner;
+import dev.sam.exchange.persistence.CommandJournal;
 
 /**
  * Hello world!
  */
 public class App {
 
-  public static void main(String[] args) {
-    PlaceOrder order = new PlaceOrder(1L, Side.BID, 101L, -5L);
-    System.out.println(order.side());
-    System.out.println(order.priceTicks());
+  public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Usage: java -cp target/classes dev.sam.exchange.App <journal-file>");
+    }
+
+    Path path = Path.of(args[0]);
+    CommandJournal journal = new CommandJournal(path);
+
+    List<EngineCommand> commands = journal.readAll();
+
+    ReplayRunner runner = new ReplayRunner();
+    ReplayResult results = runner.replay(commands);
+
+    for (CommandResult result : results.results()) {
+      System.out.println(result);
+    }
+    System.out.println(results.bookSnapshot());
   }
 }
